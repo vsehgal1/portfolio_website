2a610a6188193286bddaf07f64fc036d636e27fb
vsehgal <vikramsehgal@live.in>
2020-08-04 18:30:36 -0400

    Updates

---
+++ precache-manifest.b0c00a95b4f51c7e5b14c63f9addec31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec19856fda9d9ecd7a1a6c9db498c7a3",
+    "url": "/vsehgal1.github.io/index.html"
+  },
+  {
+    "revision": "0a687e9c70ca5a1c6351",
+    "url": "/vsehgal1.github.io/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "72ed3b6999f2f93b8fd3",
+    "url": "/vsehgal1.github.io/static/css/main.75fd5ec3.chunk.css"
+  },
+  {
+    "revision": "0a687e9c70ca5a1c6351",
+    "url": "/vsehgal1.github.io/static/js/2.9083ccbd.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/vsehgal1.github.io/static/js/2.9083ccbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72ed3b6999f2f93b8fd3",
+    "url": "/vsehgal1.github.io/static/js/main.29d0c4c9.chunk.js"
+  },
+  {
+    "revision": "6c85e654fccd25437af8",
+    "url": "/vsehgal1.github.io/static/js/runtime-main.e41285f3.js"
+  },
+  {
+    "revision": "ccba7ce9c4b25080838ee1c3eeba2512",
+    "url": "/vsehgal1.github.io/static/media/myAvatar.ccba7ce9.png"
+  }
+]);
