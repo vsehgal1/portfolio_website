c64b6cb4a5c6ce8c2dde81cde9bfeea0d523b2d7
vsehgal <vikramsehgal@live.in>
2020-08-11 12:36:32 -0400

    new update

--- src/components/project.js
+++ src/components/project.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import NpProj from './npProj';
+import TwitterProj from './twitProj';
+import DrawingRecorder from './recProj';
 export default class Project extends React.Component {
     render() {
         return (
-            <div>this is my Project</div>
+            <div>
+                <div style={{ padding: '5em', float:'left' }}>
+                    <NpProj></NpProj>
+                    <br></br>
+                    <DrawingRecorder></DrawingRecorder>
+                </div>
+                <div style={{ padding: '5em' }}>
+                    <TwitterProj></TwitterProj>
+                    <br></br>
+                    <NpProj></NpProj>
+                </div>
+            </div>
         )
     }
 }
---
+++ src/components/twitProj.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function TwitterProj() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Twitter User Search
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            JavaScript, ReactJS, NodeJS, Express
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Live Demo
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
